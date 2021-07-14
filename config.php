a0081f303b2f554a809d1f05e4d6f5509d30156d
stilocybin <73328934+stilocybin@users.noreply.github.com>
2021-07-14 15:15:21 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,11 @@
 <?php
 
-# PHP-Warnungen und Fehler ausblenden
-//error_reporting(0);
-# PHP-Notizen ausblenden
-//error_reporting( E_ALL ^ E_NOTICE );
+
 
 
 ######## Datenbank-Verbindung ##########
 
-# Angaben zur Datenbankverbindung
+
 define( 'DBHOST', 'localhost' );
 define( 'DBUSER', 'root' );
 define( 'DBPASS', 'cimdata' );
